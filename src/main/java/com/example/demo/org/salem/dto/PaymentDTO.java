c5e032826f4f675836936be902296371004649f1
mkoko8855 <mkoko8855@naver.com>
2023-07-05 18:49:14 +0900

    20230705

--- src/main/java/com/example/demo/org/salem/dto/PaymentDTO.java
+++ src/main/java/com/example/demo/org/salem/dto/PaymentDTO.java
@@ -1,10 +1,13 @@
 package com.example.demo.org.salem.dto;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 
 @Data
 @Entity
+@table()
+@Builder
 public class PaymentDTO {
     private String tid;
     private String pod;
